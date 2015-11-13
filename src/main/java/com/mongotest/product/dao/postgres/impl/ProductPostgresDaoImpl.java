16676a809be066b98248d2109a121f36641d5924
Diego Amaya <diegoaam_02@hotmail.com>
2015-11-13 01:37:39 -0500

    Se agrega JDBC de Postgres

--- src/main/java/com/mongotest/product/dao/postgres/impl/ProductPostgresDaoImpl.java
+++ src/main/java/com/mongotest/product/dao/postgres/impl/ProductPostgresDaoImpl.java
@@ -1,6 +1,7 @@
 package com.mongotest.product.dao.postgres.impl;
 
 import com.mongotest.product.dao.ProductDao;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.math.BigDecimal;
 
@@ -8,8 +9,11 @@ import java.math.BigDecimal;
  * Created by diegoamaya on 12/11/15.
  */
 public class ProductPostgresDaoImpl implements ProductDao {
-    
+
+    private JdbcTemplate jdbcTemplate;
+
     public int retrieveAllProducts() {
+        jdbcTemplate.queryForList("select * from products");
         return 0;
     }
 
@@ -19,5 +23,9 @@ public class ProductPostgresDaoImpl implements ProductDao {
 
     public int retrieveAllProductsFromSpecialCategory(String category) {
         return 0;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
